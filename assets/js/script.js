d39e4de290b43daea15d067078eec7afc49ec1cb
kotaewing <kota.ewing@gmail.com>
2021-04-06 14:20:55 -0600

    reset form and validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,20 @@ function taskFormHandler(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert('You need to fill out the task form!');
+        return false;
+    }
     
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     }
+
+    formEl.reset();
 
     // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
